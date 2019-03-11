944b42012a67d9c7aef98fd8d2d270db112ae61d
Niko Dyer <nikodyer44@gmail.com>
2019-03-11 16:54:30 -0600

    added filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import List from './List'
 import GroceryForm from './GroceryForm'
+import Footer from './Footer'
 
 class App extends React.Component {
   state = {
@@ -9,7 +10,12 @@ class App extends React.Component {
       { id: 1, name: 'Apples', complete: true },
       { id: 2, name: 'Bananas', complete: false },
       { id: 3, name: 'Cereal', complete: false }
-    ]
+    ],
+    filter: 'All'
+  }
+
+  setFilter = (filter) => {
+    this.setState({ filter })
   }
 
   getId = () => {
@@ -39,13 +45,26 @@ class App extends React.Component {
     })
   }
 
+  visibleItems = () => {
+    const { groceries, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return groceries.filter( t => !t.complete)
+      case 'Complete':
+        return groceries.filter( t => t.complete)
+      default: 
+        return groceries
+    }
+  }
+
   render() {
-    const { groceries } = this.state
+    const { filter } = this.state
 
     return (
       <div>
         <GroceryForm addItem={this.addItem} />
-        <List name='Grocery List' items={groceries} groceryClick={this.handleClick} />
+        <List name='Grocery List' items={this.visibleItems()} groceryClick={this.handleClick} />
+        <Footer filter={filter} setFilter={this.setFilter} />
       </div>
     )
   }
